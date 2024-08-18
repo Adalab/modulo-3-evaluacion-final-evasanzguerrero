7b4b51e5250f35d150ba722b92bddc33084bab4d
evasanzgue <evasanzgue@gmail.com>
2024-08-18 17:16:25 +0200

    I've created the HTML structure.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,32 @@
 import '../styles/App.scss';
 
+import castilloHarry from '../../src/images/castillo-harry.webp'
+
 function App() {
   return (
-    <>
-      Aquí va tu HTML
-    </>
+    <div>
+      <header>
+        <img src={castilloHarry} alt="Imagen castillo de Harry Potter"></img>
+      </header>
+      <main>
+        <form>
+          <label htmlFor="search">Busca por personaje: </label>
+          <input type="text" name="Search" id="Search" />
+          <div>
+            <label htmlFor="house">Selecciona una casa: </label>
+            <select name="house" id="house">
+              <option value="">🦉</option>
+              <option value="gr">Gryffindor</option>
+              <option value="sl">Slytherin</option>
+              <option value="hu">Hufflepuff</option>
+              <option value="ra">Ravenclaw</option>
+            </select>
+          </div>
+        </form>
+        <section></section>
+      </main>
+      <footer> 🧹 Adalab promo Betty 2024 🧹</footer>
+    </div>
   )
 }
 
