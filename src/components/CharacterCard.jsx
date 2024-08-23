b7cab6bd010494278c88782f55eb780eace970cd
evasanzgue <evasanzgue@gmail.com>
2024-08-23 15:53:08 +0200

    Add CSS in the details section

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 function CharacterCard({character}) {
   return (
     <Link to={`/detail/${character.id}`} className="character">
-      <img
-        className="character_img"
-        src={
-          character.image ||
-          "https://www.lavanguardia.com/andro4all/hero/2024/02/harry-potter.1708702569.2268.jpg?width=768&aspect_ratio=16:9&format=nowebp"
-        }
-        alt={`Imagen de ${character.name}`}
-      />
-      <div className="character_info">
-        <h2 className="character">{character.name}</h2>
-        <p className="character">{character.species}</p>
-      </div>
+      
+        <img
+          className="character_img"
+          src={
+            character.image ||
+            "https://www.lavanguardia.com/andro4all/hero/2024/02/harry-potter.1708702569.2268.jpg?width=768&aspect_ratio=16:9&format=nowebp"
+          }
+          alt={`Imagen de ${character.name}`}
+        />
+        <div className="character_info">
+          <h2 className="character">{character.name}</h2>
+          <p className="character">{character.species}</p>
+        </div>
+      
     </Link>
   );
 }
--- src/components/layout/Detail.jsx
+++ src/components/layout/Detail.jsx
@@ -7,25 +7,28 @@ function Detail({findCharacterByID}) {
   const character = findCharacterByID(params.id);
   console.log(character)
 
-  const termination = character.gender === 'famale' ? 'a':'o'
+  const termination = character.gender === 'famale' ? 'a':'o';
   return (
     <>
-      <Link to="/">Volver</Link>
+    
+    <div className="container_details">
+      <Link to="/"  className="return" >Volver</Link>
       <img
-        className="character_img"
+        className="character_img info_img"
         src={
           character.image ||
           "https://www.lavanguardia.com/andro4all/hero/2024/02/harry-potter.1708702569.2268.jpg?width=768&aspect_ratio=16:9&format=nowebp"
         }
         alt={`Imagen de ${character.name}`}
       />
-      <div>
-        <p>Nombre: {character.name}</p>
-        <p>Especie: {character.species}</p>
-        <p>Estatus: {character.alive ? `Viv${termination}`: `Muert${termination}`}</p>
-        <p>Genero: {character.gender}</p>
-        <p>Casa: {character.house}</p>
-      </div>
+      <article className="info_detail">
+        <p className="details">Nombre: {character.name}</p>
+        <p className="details">Especie: {character.species}</p>
+        <p className="details">Estatus: {character.alive ? `Viv${termination}`: `Muert${termination}`}</p>
+        <p className="details">Genero: {character.gender}</p>
+        <p className="details">Casa: {character.house}</p>
+      </article>
+    </div>
     </>
   );
 }
